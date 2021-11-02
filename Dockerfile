7aa723ca6dd1bd0260e0ac1a3e4f71b614a9030a
kavidj <suhashiniv1402@gmail.com>
2021-11-02 11:51:38 +0530

    updated the folder

--- Dockerfile
+++
@@ -1,3 +0,0 @@
-FROM nginx
-COPY . C:\Users\Nusyn\Desktop\Dockerfile\index.html
-EXPOSE 90:8080
--- notes.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-docker build -t nginx:1.0 .
-
-
-https://releases.hashicorp.com/terraform/1.0.7/terraform_1.0.7_linux_amd64.zip
